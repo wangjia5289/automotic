bacc891559611a3ca94949ae8ac926bd9f7a0859
wangjia <wangjia5289@163.com>
2025-04-06 13:39:36 +0800

    sum

---
+++ src/main/java/com/example/automotic/AutomoticApplication.java
@@ -0,0 +1,13 @@
+package com.example.automotic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomoticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomoticApplication.class, args);
+    }
+
+}
